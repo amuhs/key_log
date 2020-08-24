7681bd05a0f1bd6fdfd3a3f1cf5a48f638b5b1b7
Andrew Muhs <muhsandrew@gmail.com>
2020-08-24 17:18:32 -0500

    Major over haul (again).
    - Since I am not using something like React I have added tamplates so I don't need to worry about updating things like the navigation in multiple places.
    - I have also reworked some of the API so it is shorter and a bit more read able.
    - Material Lite has been removed and I have added some styling with flex box.
    
    Here's to hoping these commits will contain slightly less and wil be more to the point.

--- index.js
+++ index.js
@@ -1,87 +1,191 @@
 const express = require("express");
 const Datastore = require("nedb");
+const nunjucks = require("nunjucks");
+const jsonschema = require("jsonschema");
 
 const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
 const db = new Datastore({ filename: "key_log.db" });
-db.loadDatabase(err => {
-    if (err !== null) {
-        console.error("Problem on DB load: ");
-        console.error(err);
+db.loadDatabase((err) => {
+  if (err !== null) {
+    console.error("Problem on DB load: ");
+    console.error(err);
+  }
+});
+
+db.insert({ label: "12345", available: true });
+db.insert({ label: "67890", available: false });
+db.insert({ label: "abcd edfg", available: true });
+
+const schema = {
+  type: "object",
+  properties: {
+    label: {
+      type: "string",
+      minLength: 3,
+      maxLength: 30,
+    },
+    available: {
+      type: "boolean",
+    },
+  },
+  required: ["label", "available"],
+};
+
+/**
+ *
+ * Page Routes
+ *
+ */
+app.get("/", (req, res) => {
+  res.render("index.njk");
+});
+
+app.get("/about/", (req, res) => {
+  res.render("about.njk");
+});
+
+app.get("/manage/", (req, res) => {
+  res.render("manage.njk");
+});
+
+/**
+ *
+ * API Routes
+ *
+ */
+
+// Gets all keys in the DB
+app.get("/keys", (request, response) => {
+  db.find({}, (err, keys) => {
+    if (err) {
+      console.error("Sending failure to client...");
+      response.json({
+        error: {
+          code: 500,
+          message: "Failed to retrieve keys.",
+        },
+      });
+      response.end();
+    } else {
+      console.log("Sending success to client...");
+      response.json({
+        data: keys,
+      });
+      response.end();
     }
+  });
 });
 
-app.get("/api/v1/key", (request, response) => {
-    console.log("All keys requested.");
-    db.find({}, (err, keys) => {
-        response.json(keys);
-    });
+// Gets a specific key by _id
+app.get("/keys/:id", (request, response) => {
+  const keyID = request.params.id;
+  db.find({_id: keyID}, (err, key) => {
+    if (err) {
+      console.error("Sending failure to client...");
+      response.json({
+        error: {
+          code: 500,
+          message: `Failed to retrieve key ${keyID}.`,
+        },
+      });
+      response.end();
+    } else {
+      console.log("Sending success to client...");
+      response.json({
+        data: key,
+      });
+      response.end();
+    }
+  });
 });
 
+// Creates a new key
+app.post("/keys", (request, response) => {
+  let valid = jsonschema.validate(request.body, schema);
 
-app.get("/api/v1/key/exists/:id", (request, response) => {
-    console.log("Adding new key to db...");
-    console.log(`Checking for ${request.params.id}...`);
-    db.find({ key_id: request.params.id }, (err, result) => {
-        if (err) {
-            data = {};
-        } else {
-            if (result === undefined || result.length == 0) {
-                response.send({keyExists: false});
-                response.end();
-            } else {
-                response.send({keyExists: true});
-                response.end();
-            }
-        }
-    });
-});
-
-
-app.post("/api/v1/key/add", (request, response) => {
-    console.log("Adding new key to db...");
-    db.insert({ 
-        key_id: parseInt(request.body.id),
-        key_desc: request.body.desc,
-        key_avail: true,
-        time_removed: null,
-        created_ms: Date.now()
-    }, (err, newDoc) => {
-        if (err) {
-            console.error("Sending failure to client...");
-            const data = {
-                status: "FAIL"
-            };
-            response.json(data);
-            response.end();
-        } else {
-            console.log("Sending success to client...");
-            const data = {
-                status: "SUCCESS",
-                newKey: newDoc
-            };
-            response.json(data);
-            response.end();
-        }
-    });
-});
-
-
-app.post("/api/v1/key/edit", (request, response) => {
+  if (!valid) {
+    console.error("Sending failure to client...");
     response.json({
-        status: "SUCCESS"
+      error: {
+        code: 400,
+        message: "JSON failed validation. Only `label` and `available` allowed.",
+      },
     });
     response.end();
+  }
+
+  db.insert(request.body, (err, newDoc) => {
+    if (err) {
+      console.error("Sending failure to client...");
+      response.json({
+        error: {
+          code: 500,
+          message: "Failed to create key.",
+        },
+      });
+      response.end();
+    } else {
+      console.log("Sending success to client...");
+      response.json({
+        data: newDoc,
+      });
+      response.end();
+    }
+  });
 });
 
+// Modifies a key
+app.put("/keys/:id", (request, response) => {
+  let valid = jsonschema.validate(request.body, schema);
 
-app.post("/api/v1/key/delete", (request, response) => {
+  if (!valid) {
+    console.error("Sending failure to client...");
     response.json({
-        status: "SUCCESS"
+      error: {
+        code: 400,
+        message: "JSON failed validation. Only `label` and `available` allowed.",
+      },
     });
     response.end();
+  }
+
+  db.update({ _id: request.params.id }, { $set: request.body }, {}, (err, numReplaced, upsert) => {
+    if (err) {
+      console.error("Sending failure to client...");
+      response.json({
+        error: {
+          code: 500,
+          message: "Failed to update key.",
+        },
+      });
+      response.end();
+    } else {
+      console.log("Sending success to client...");
+      response.json({
+        data: `Updated ${numReplaced} key.`,
+      });
+      response.end();
+    }
+  });
+});
+
+// Removes a single key from the DB
+app.delete("/keys/:id", (request, response) => {
+  // TODO
+});
+
+// Removes all keys in the supplied JSON
+app.delete("/keys", (request, response) => {
+  // TODO
 });
---
+++ public/js/delete.js
@@ -0,0 +1,43 @@
+async function handleClick(event) {
+  const isCheckedOut = event.target.classList.contains("checked-out");
+  
+  const putResult = await fetch(`/keys/${event.target.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      label: event.target.textContent,
+      available: isCheckedOut,
+    }),
+  }).then((response) => response.json());
+
+  const root = document.getElementById("root");
+  while (root.firstChild) {
+    root.removeChild(root.lastChild);
+  }
+  getKeys();
+}
+
+async function getKeys() {
+  let keys = await fetch("/keys").then((response) => response.json());
+  keys = keys.data.sort();
+  const root = document.getElementById("root");
+
+  for (key of keys) {
+    let newButton = document.createElement("button");
+    newButton.className = "key";
+    newButton.type = "button";
+    newButton.textContent = `${key.label}`;
+    newButton.id = key._id;
+    newButton.onclick = (e) => {
+      handleClick(e);
+    };
+
+    if (!key.available) {
+      newButton.classList.add("checked-out");
+    }
+
+    root.append(newButton);
+  }
+}
+
+getKeys();
